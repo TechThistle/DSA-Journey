2e49b5f81ff2f478f1e2989509e87fbb330ef0ca
Dimple <dimple4122004@gmail.com>
2026-06-13 13:59:54 +0530

    Recursion Program

--- Recursion/binarysearch.cpp
+++ Recursion/binarysearch.cpp
@@ -24,6 +24,6 @@ int binarysearch(vector<int> &arr, int tar, int st, int end)
 int main()
 {
     vector<int> arr={1, 2, 3, 4, 5};
-    cout<<binarysearch(arr, 6, 0, arr.size()-1)<<endl;
+    cout<<binarysearch(arr, 3, 0, arr.size()-1)<<endl;
     return 0;
 }
---
+++ subsets1.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+void subsets(vector<int> &arr, vector<int> &ans, int i)
+{
+    int n=arr.size();
+    if(i==n)
+    {
+        for(int val: ans)
+        {
+            cout<<val<<" ";
+        }
+        cout<<endl;
+        return;
+    }
+    ans.push_back(arr[i]);
+    subsets(arr, ans, i+1);
+    ans.pop_back();
+    subsets(arr, ans, i+1);
+}
+int main()
+{
+    vector<int> arr={1, 2, 3};
+    vector<int> ans;
+    for(int a=0; a<ans.size(); a++)
+    {
+        if(ans)
+    }
+    subsets(arr, ans, 0);
+    return 0;
+}
